fc08637f1a9dc28e75841e52cae8ec3d11aeaee2
BuiVietAnhTLU <99933040+BuiVietAnhTLU@users.noreply.github.com>
2022-03-29 10:13:22 +0700

    app.js

--- app.js
+++ app.js
@@ -51,10 +51,3 @@ checkOnly(txEmail, email, statusOfEmail, 'Vui lòng nhập đúng email')
 // }
 
 // txName.onblur = checkName/ const checkName = () => {
-// 	const valueName = alphabate.test(txName.value)
-// 	valueName
-// 		? (statusOfName.textContent = undefined)
-// 		: (statusOfName.textContent = 'Phải là chữ k dấu')
-// }
-
-// txName.onblur = checkName
